eaae59c3c7e994e02bf901c6b3e7580c4377ca18
Luis Reis Viera <luantorv@gmail.com>
2026-02-19 21:01:33 -0300

    Starship Design 2: Revenge

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -7,34 +7,37 @@
   
     settings = {
       format = ''
-      [$username]($style) [$directory]($style) $git_branch $nix_shell
+      [](${config.colors.skyblue})[ $username ](bold black bg:${config.colors.skyblue})[](${config.colors.skyblue}) [](${config.colors.pink})[ $directory ](bold black bg:${config.colors.pink})[](${config.colors.pink}) $git_branch$nix_shell
       $character '';
     
       username = {
-        style_user = "bold ${config.colors.skyblue}";
+        style_user = "bold ${config.colors.black} bg:${config.colors.skyblue}";
+        format = "[$user]($style)";
         show_always = true;
       };
 
       directory = {
-        style = "bold ${config.colors.pink}";
+        style = "bold ${config.colors.black} bg:${config.colors.pink}";
+        format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "…/";
       };
 
       character = {
-        success_symbol = "[➜](${config.colors.white})";
+        success_symbol = "[➜](${config.colors.darkblue})";
         error_symbol = "[➜](${config.colors.red})";
       };
 
       git_branch = {
         symbol = " ";
-        style = "bold ${config.colors.pink}";
+        style = "bold ${config.colors.black} bg:${config.colors.white}";
+        format = "[](${config.colors.white})[ $symbol$branch ]($style)[](${config.colors.white})";
       };
 
       nix_shell = {
         symbol = " ";
-        style = "bold ${config.colors.darkblue}";
-        format = "via [$symbol$state]($style) ";
+        style = "bold ${config.colors.black} bg:${config.colors.white}";
+        format = "[](${config.colors.white})[ $symbol$state ]($style)[](${config.colors.white})";
       };
     };
   };
