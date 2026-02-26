426614e78bc5cbba9b473252ae014d346d52d2f3
Luis Reis Viera <luantorv@gmail.com>
2026-02-26 19:54:48 -0300

    refactor: add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      globalVars = {
+        username = "luis";
+        homeDirectory = "/home/luis";
+        system = "x86_64-linux";
+      };
+    in
+    {
+      nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit (globalVars) system;
+          specialArgs = { inherit globalVars; } // { inherit home-manager; };
+          modules = [
+            home-manager.nixosModules.home-manager
+            ./hosts/laptop/default.nix
+          ];
+        };
+      };
+
+      devShells.${globalVars.system} = import ./shells {
+        pkgs = nixpkgs.legacyPackages.${globalVars.system};
+      };
+    };
+}
