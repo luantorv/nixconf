e9b5a8eba1de6fcdb7f221491da153db6f001a6f
Luis Reis Viera <luantorv@gmail.com>
2026-03-01 19:39:28 -0300

    config firewall

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -6,4 +6,15 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ ];
+    allowedUDPPorts = [ ];
+  };
+
+  networking.firewall.logRefusedConnections = true;
+
+  services.avahi.enable = false;
+  services.printing.enable = false;
 }
