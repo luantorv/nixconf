f73b0f1db13b945800960a4342e09352eb4631ce
Luis Reis Viera <luantorv@gmail.com>
2026-04-28 17:29:20 -0300

    Add zram

--- hosts/laptop/boot.nix
+++ hosts/laptop/boot.nix
@@ -7,4 +7,11 @@
     "intel_pstate=active" 
     "processor.max_cstate=1" 
   ];
+
+  # Add zram
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 25;
+  };
 }
--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -8,12 +8,6 @@
     ../../profiles/desktop.nix
   ];
 
-  fileSystems."/data" = {
-    device = "/dev/disk/by-uuid/b7cda332-f5a9-40ff-82e4-bb9ad18478c1";
-    fsType = "ext4";
-    options = [ "defaults" "nofail" ];
-  };
-
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
