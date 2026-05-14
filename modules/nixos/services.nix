ebaf67317265b69c26b9d9597550e9a2641ca98c
Luis Reis Viera <luantorv@gmail.com>
2026-05-14 19:33:35 -0300

    adapt tuigreet to manage multiple WM/DE

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -29,7 +29,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd river";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
         user = "${globalVars.username}";
       };
     };
