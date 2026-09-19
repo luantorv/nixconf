76075f6631daa366f2040078f9bc2269ff0b9df4
Luis Reis Viera <luantorv@gmail.com>
2026-09-19 09:25:16 -0300

    activate Silent Boot on laptop

--- modules/nixos/boot/grub.nix
+++ modules/nixos/boot/grub.nix
@@ -4,16 +4,34 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Bootloader.
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    useOSProber = true;
-    device = "nodev";
-  };
-  
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        useOSProber = true;
+        device = "nodev";
+      };
 
-  # Kernel por defecto (fallback). Un host puede sobrescribirlo en su boot.nix.
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
+      # theme = "spinner";
+    };
+
+    # Silent boot
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
 }
