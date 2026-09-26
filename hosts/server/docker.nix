b34308fa1a6fb4e81ed01caf138e0e48f03916f1
Luis Reis Viera <luantorv@gmail.com>
2026-09-26 09:16:03 -0300

    Simplify server config for docker

--- hosts/server/docker.nix
+++ hosts/server/docker.nix
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2026 Luis
+# SPDX-FileCopyrightText: 2026 Luis Reis Viera
 # SPDX-License-Identifier: Apache-2.0
 
 { config, pkgs, ... }:
@@ -6,34 +6,12 @@
 {
   virtualisation.docker.enable = true;
 
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      "ollama" = {
-        image = "ollama/ollama:latest";
-        volumes = [ "ollama_data:/root/.ollama" ];
-        ports = [ "11434:11434" ];
-      };
+  virtualisation.libvirtd.enable = true;
 
-      "open-webui" = {
-        image = "ghcr.io/open-webui/open-webui:main";
-        ports = [ "3000:8080" ];
-        
-        environmentFiles = [
-          config.sops.secrets.open_webui_secret.path
-        ];
+  environment.systemPackages = with pkgs; [
+    docker-compose
+    lazydocker
+  ];
 
-        environment = {
-          "OLLAMA_BASE_URL" = "http://host.docker.internal:11434";
-        };
-
-        extraOptions = [ "--add-host=host.docker.internal:host-gateway" ];
-
-        volumes = [ "open-webui_data:/app/backend/data" ];
-        dependsOn = [ "ollama" ];
-      };
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [ 3000 11434 ];
+  users.users.${globalVars.username}.extraGroups = [ "docker" ];
 }
